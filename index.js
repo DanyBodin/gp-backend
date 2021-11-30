4144b09069b64ccbe845c33dfcb29f0bded1cab8
DANY <dany@mbp-de-dany.lan>
2021-11-30 13:20:01 +0100

    server up

---
+++ index.js
@@ -0,0 +1,53 @@
+const express = require("express");
+const formidableMiddleware = require("express-formidable");
+const cors = require("cors");
+const axios = require("axios");
+const mongoose = require("mongoose");
+
+const isAuthentificated = require("./middlewares/isAuthentificated");
+
+require("dotenv").config();
+
+mongoose.connect(process.env.MONGO_ATLAS_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const app = express();
+app.use(formidableMiddleware());
+app.use(cors());
+
+app.get("/", async (req, res) => {
+  try {
+    const response = await axios.get(
+      `${process.env.GP_URI}/games?key=${process.env.GP_APIKEY}`
+    );
+    console.log(response.data);
+    res.status(200).json(response.data);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
+
+const userRoutes = require("./routes/user");
+app.use(userRoutes);
+
+/*
+app.get("/signup", isAuthentificated, async (req, res) => {
+  try {
+  } catch (error) {}
+});
+
+app.get("/login", isAuthentificated, async (req, res) => {
+  try {
+  } catch (error) {}
+});
+*/
+
+app.all("*", (req, res) => {
+  res.json("All Routes");
+});
+
+app.listen(process.env.PORT, () => {
+  console.log("server up and running");
+});
